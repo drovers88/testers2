9a3effb4f2a5659af41cc67ee7ff8cdbb81fa784
Kenneth Fee <drovers88@gmail.com>
2017-05-22 14:52:01 -0700

    first commit

---
+++ tester2/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  tester2
+//
+//  Created by Kenneth Fee on 5/21/17.
+//  Copyright © 2017 Kenneth Fee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var FirstLabel: UILabel!
+    
+    var tapcount = 0
+    
+    @IBAction func ButtonPress(sender: AnyObject) {
+        
+        tapcount = tapcount+1
+        print (tapcount)
+        
+        FirstLabel.text="Hola"
+        
+        if tapcount >= 10 { FirstLabel.text = "WHOOOP"
+        }
+    }
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
